727d432ec44e3b93892e453174dfd2cdec70da05
alex5667 <alex56671111@gmail.com>
2024-03-27 18:33:38 +0200

    feat: add mylogger

--- src/logger/logger.service.ts
+++ src/logger/logger.service.ts
@@ -1,4 +1,42 @@
-import { Injectable } from '@nestjs/common';
-
+import { Injectable, LogLevel, LoggerService } from '@nestjs/common';
 @Injectable()
-export class LoggerService {}
+export class MyLogger implements LoggerService {
+  log(message: any, ...optionalParams: any[]) {
+    console.log('\x1b[1m\x1b[32m' + 'log', message, '\x1b[0m', optionalParams);
+  }
+  error(message: any, ...optionalParams: any[]) {
+    console.error(
+      '\x1b[1m\x1b[31m' + 'error',
+      message,
+      optionalParams,
+      '\x1b[0m',
+    );
+  }
+  warn(message: any, ...optionalParams: any[]) {
+    console.warn(
+      '\x1b[1m\x1b[33m' + 'warn',
+      message,
+      optionalParams,
+      '\x1b[0m',
+    );
+  }
+  debug?(message: any, ...optionalParams: any[]) {
+    console.debug(
+      '\x1b[1m\x1b[35m' + 'debug',
+      message,
+      optionalParams,
+      '\x1b[0m',
+    );
+  }
+  verbose?(message: any, ...optionalParams: any[]) {
+    console.log(
+      '\x1b[1m\x1b[32m' + 'verbose',
+      message,
+      optionalParams,
+      '\x1b[0m',
+    );
+  }
+  setLogLevels?(levels: LogLevel[]) {
+    console.log('\x1b[1m\x1b[32m' + 'levels', levels);
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,13 @@ import { readFile } from 'fs/promises';
 import { resolve } from 'path';
 import * as yaml from 'js-yaml';
 import { SwaggerModule } from '@nestjs/swagger';
+import { MyLogger } from './logger/logger.service';
 config();
 
 async function bootstrap() {
   const PORT = Number(process.env.PORT) || 4000;
   const app = await NestFactory.create(AppModule);
+
   try {
     const filePath = resolve(__dirname, '..', 'doc', 'api.yaml');
     const yamlFile = await readFile(filePath, 'utf8');
@@ -26,6 +28,7 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+  app.useLogger(app.get(MyLogger));
   await app.listen(PORT, () => {
     console.log(`Server listen on port ${PORT}`);
   });
